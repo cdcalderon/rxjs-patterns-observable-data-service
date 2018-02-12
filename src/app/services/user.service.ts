e9da0efdb8643adc28ba0c9d4bffc26c2336b62c
carloscalderon <cdcalderon@gmail.com>
2018-02-12 06:12:16 -0600

    Implement login functionality using Observable data service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable} from 'rxjs/';
 import {User} from '../shared/model/user';
+import {Http} from '@angular/http';
 
 export const UNKNOWN_USER: User = {
   firstName: 'Unknown'
@@ -11,6 +12,19 @@ export class UserService {
 
   user$: Observable<User> = Observable.of(UNKNOWN_USER);
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  login(email: string, password: string) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    this.http.post('/api/login', {email, password})
+      .subscribe(
+        user => {
+
+        },
+        () => alert('Login Failed')
+      );
+  }
 
 }
